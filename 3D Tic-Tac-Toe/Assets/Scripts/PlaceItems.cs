d52484d58456337d22b2e0e7ca15bdd1b3a6a519
DexterBot <olaruandreidan@gmail.com>
2024-02-11 21:35:32 +0200

    Refactor whole game logic cuz it was trash

--- 3D Tic-Tac-Toe/Assets/Scripts/PlaceItems.cs
+++ 3D Tic-Tac-Toe/Assets/Scripts/PlaceItems.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using System;
 
 public class PlaceItems : MonoBehaviour
 {
@@ -36,24 +37,25 @@ public class PlaceItems : MonoBehaviour
     }
 
     // Update is called once per frame
-   private void Update()
-    {
-        if(CheckWin('X')||CheckWin('Y')||CheckIfDone())
+  private void Update()
+{
+   if (currentPlayer =='Y'||currentPlayer=='X') //this is for 2 AIs, will change later
+   {
+        secondsToNext-=Time.deltaTime;
+        if (secondsToNext<=0)
+        {
+            AIPlay();
+            if (CheckWin()||CheckIfDone())
             {
                 //DisplayWinMessage
                 UnityEditor.EditorApplication.ExitPlaymode();
             }
-        
-        //if (currentPlayer=='Y')  //disable this if you want to actually play the game
-        secondsToNext -= Time.deltaTime;  // T.dt is secs since last update
-        if(secondsToNext<=0)
-        {
-            AIPlay();
             SwitchPlayer();
             secondsToNext = 2;
         }
-              
-    }
+   }
+    
+}
 
     void InitializeGameBoard()
     {
@@ -63,132 +65,244 @@ public class PlaceItems : MonoBehaviour
                     gameBoard[i,j,k] ='-';
     }
 
-    public bool CheckWin(char player) 
+    public bool CheckWin()
     {
-        
-        for (int i = 0; i < 4; i++) // Check rows and columns in each layer, and verticals across layers
-        {
-            if (CheckLine(player, i, 0, 0, 0, 1, 0) || // Rows in each layer
-                CheckLine(player, 0, i, 0, 1, 0, 0) || // Columns in each layer
-                CheckLine(player, 0, 0, i, 0, 0, 1))   // Columns across layers
-                return true;
-        }
-
-            for (int z = 0; z < 4; z++) // Check diagonals in each layer
-        {
-            if (CheckLine(player, 0, 0, z, 1, 1, 0) || // Diagonal from top-left to bottom-right in each layer
-                CheckLine(player, 3, 0, z, -1, 1, 0))  // Diagonal from top-right to bottom-left in each layer
-                return true;
-        }
-
-        // Check 3D diagonals
-        if (CheckLine(player, 0, 0, 0, 1, 1, 1) || // Diagonal from top-left-front to bottom-right-back
-            CheckLine(player, 3, 0, 0, -1, 1, 1) || // Diagonal from top-right-front to bottom-left-back
-            CheckLine(player, 0, 3, 0, 1, -1, 1) || // Diagonal from bottom-left-front to top-right-back
-            CheckLine(player, 3, 3, 0, -1, -1, 1))  // Diagonal from bottom-right-front to top-left-back
-            return true;
-
-        // Add checks for vertical lines across layers and any other missing orientations
-        for (int x = 0; x < 4; x++)
-            for (int y = 0; y < 4; y++)
-                if (CheckLine(player, x, y, 0, 0, 0, 1)) // Vertical lines across layers
-                    return true;
-
-        return false;
-    }
-
-    private bool CheckLine(char player, int startX, int startY, int startZ, int dx, int dy, int dz, bool allowEmpty = false)
-    {
-        int playerCount = 0;
-        int emptyCount = 0;
-        for (int i = 0; i < 4; i++)
-        {
-            int x = startX + i * dx;
-            int y = startY + i * dy;
-            int z = startZ + i * dz;
-            // Check if the indices are within the bounds of the game board
-            if (x >= 0 && x < 4 && y >= 0 && y < 4 && z >= 0 && z < 4)
+        for (int x = 0; x < 4; x++) 
             {
-                char symbol = gameBoard[x, y, z];
-                if (symbol == player) playerCount++;
-                else if (symbol == '-') emptyCount++;
-                else return false; // Line contains opponent's symbol
-            }
-            else return false;
-        }
-        return allowEmpty ? playerCount > 0 : playerCount == 4;
-    }
-
-
-    public void AIPlay()
-    {
-        // First, try to block the opponent's immediate win.
-        if (TryToBlockOpponent())
-            return;
-
-        // If no block is needed, attempt to make a strategic move.
-        if (MakeStrategicMove())
-            return;
-
-        // Fallback to a random move if no strategic move is found.
-        MakeRandomMove();
-    }
-
-    private bool TryToBlockOpponent()
-    {
-        char opponent = currentPlayer == 'X' ? 'Y' : 'X';
-        for (int x = 0; x < 4; x++)
-        {
-            for (int y = 0; y < 4; y++)
-            {
-                for (int z = 0; z < 4; z++)
+                for (int y = 0; y < 4; y++) 
                 {
-                    if (gameBoard[x, y, z] == '-')
+                    for (int z = 0; z < 4; z++) 
                     {
-                        gameBoard[x, y, z] = opponent;
-                        if (CheckWin(opponent))
-                        {
-                            gameBoard[x, y, z] = currentPlayer; // Block the opponent
-                            PlaceSymbol(x, y, z);
+                        if (CheckCube(x, y, z)=="win")
                             return true;
-                        }
-                        gameBoard[x, y, z] = '-'; // Reset if not a winning move
                     }
                 }
             }
-        }
         return false;
     }
-    private bool MakeStrategicMove()
+
+        private string CheckCube (int x, int y, int z)
     {
-        for (int x = 0; x < 4; x++)
+        int XplayerCount=0;
+        int YplayerCount=0;
+        bool alive = false;
+        for (int i=0; i<4; i++) //check line on X axis
         {
-            for (int y = 0; y < 4; y++)
+            if (gameBoard[i, y, z] =='X')
+                XplayerCount++;
+            else if (gameBoard[i,y,z]=='Y')
+                YplayerCount++;
+
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if (i==3 && ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X')))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        for (int i=0; i<4; i++) //check line on Y axis
+        {
+            if (gameBoard[x, i, z] =='X')
+                XplayerCount++;
+            else if (gameBoard[x,i,z]=='Y')
+                YplayerCount++;
+
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if (i==3 && ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X')))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        for (int i=0; i<4; i++) //check line on z axis
+        {
+            if (gameBoard[x, y, i] =='X')
+                XplayerCount++;
+            else if (gameBoard[x,y,i]=='Y')
+                YplayerCount++;
+
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if (i==3 && ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X')))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        if ((x==0 || x==3)&&(y==0||y==3)) //if on a xy diagonal, check it
+        {
+            for (int i=0; i<4; i++)
             {
-                for (int z = 0; z < 4; z++)
+                if (gameBoard[Math.Abs(x-i), Math.Abs(y-i), z]=='X')
+                    XplayerCount++;
+                if (gameBoard[Math.Abs(x-i), Math.Abs(y-i), z]=='Y')
+                    YplayerCount++;
+            }
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X'))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        if ((x==0 || x==3)&&(z==0||z==3)) //if on a xz diagonal, check it
+        {
+            for (int i=0; i<4; i++)
+            {
+                if (gameBoard[Math.Abs(x-i), y, Math.Abs(z-i)]=='X')
+                    XplayerCount++;
+                if (gameBoard[Math.Abs(x-i), y, Math.Abs(z-i)]=='Y')
+                    YplayerCount++;
+            }
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X'))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        if ((y==0 || y==3)&&(z==0||z==3)) //if on a yz diagonal, check it
+        {
+            for (int i=0; i<4; i++)
+            {
+                if (gameBoard[x, Math.Abs(y-i), Math.Abs(z-i)]=='X')
+                    XplayerCount++;
+                if (gameBoard[x, Math.Abs(y-i), Math.Abs(z-i)]=='Y')
+                    YplayerCount++;
+            }
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X'))
+                alive = true;
+        }
+
+        XplayerCount=0;
+        YplayerCount=0;
+        if ((x==0 || x==3)&&(z==0||z==3)&&(y==0||y==3)) //it on a 3D diagonal
+        {
+            for (int i=0; i<4; i++)
+            {
+                if (gameBoard[Math.Abs(x-i), Math.Abs(y-i), Math.Abs(z-i)]=='X')
+                    XplayerCount++;
+                if (gameBoard[Math.Abs(x-i), Math.Abs(y-i), Math.Abs(z-i)]=='Y')
+                    YplayerCount++;
+            }
+            if (XplayerCount==4)
+                return "Xwin";
+            if (YplayerCount==4)
+                return "Ywin";
+            if (XplayerCount==3 && YplayerCount==0)
+                return "Xcritical";
+            if (YplayerCount ==3 && YplayerCount ==0)
+                return "Ycritical";
+            if ((XplayerCount==0 && currentPlayer=='Y') || (YplayerCount==0 && currentPlayer=='X'))
+                alive = true;
+        }
+        
+        if (alive == true)
+            return "alive";
+            else return "dead";
+    }
+
+
+
+   private void AIPlay()
+    {
+        int moveCoordinates=CriticalMove();
+        if (moveCoordinates!=0)
+        {
+            
+            gameBoard[moveCoordinates/100, moveCoordinates/10%10, moveCoordinates%10] = currentPlayer;
+            PlaceSymbol(moveCoordinates/100,moveCoordinates/10%10,moveCoordinates%10);
+        }
+        else 
+        {
+            moveCoordinates = AliveMove();
+            if (moveCoordinates!=0)
+            {
+                gameBoard[moveCoordinates/100, moveCoordinates/10%10, moveCoordinates%10] = currentPlayer;
+                PlaceSymbol(moveCoordinates/100,moveCoordinates/10%10,moveCoordinates%10);
+            }
+            else MakeRandomMove();
+        }
+        
+    }
+
+    private int CriticalMove()
+{
+    for (int x = 0; x < 4; x++) 
+        {
+            for (int y = 0; y < 4; y++) 
+            {
+                 for (int z = 0; z < 4; z++) 
                 {
-                    if (gameBoard[x, y, z] == '-')
-                    {
-                        // Temporarily mark the position with the current player's symbol
-                        gameBoard[x, y, z] = currentPlayer;
-                        // Check all directions from this position to find a strategic move
-                        if (IsPartOfWinningLine(x, y, z, currentPlayer))
-                        {
-                            PlaceSymbol(x, y, z);
-                            // No need to revert the mark as it's a valid strategic move
-                            return true; // Indicate a strategic move has been made
-                        }
-                        else
-                        {
-                            // Revert the temporary mark if it's not a strategic move
-                            gameBoard[x, y, z] = '-';
-                        }
-                    }
+                    if (gameBoard[x,y,z]=='-')
+                        if (CheckCube(x, y, z)=="Xcritical"||CheckCube(x,y,z)=="YCritical")
+                            {
+                                return x*100+y*10+z;
+                            }
                 }
             }
         }
-        return false; // No strategic move found
-    }
+    return 0;
+}
+    private int AliveMove()
+{
+    int count = 0;
+    int x = 0;
+    int y=0;
+    int z = 0;
+    do
+    {
+        count++;
+        x = random.Next(4);
+        y = random.Next(4);
+        z = random.Next(4);
+        if ((currentPlayer == 'X' && CheckCube(x,y,z)=="Xalive")||(currentPlayer=='Y' && CheckCube(x,y,z)=="Yalive"))
+            return x+100+y*10+z;
+    }   while (count<=120);
+            return 0;   
+}
 
      private void MakeRandomMove()
     {
@@ -205,39 +319,6 @@ public class PlaceItems : MonoBehaviour
             }
         }
     }
-    private bool IsPartOfWinningLine(int x, int y, int z, char player)
-    {
-        char opponent = player == 'X' ? 'Y' : 'X';
-        // Check horizontal, vertical, and both types of diagonal lines in each layer
-        // and the vertical lines across layers for potential winning moves.
-
-        for (int i = 0; i < 4; i++) // Horizontal and vertical lines in layer and across layers
-        {
-            if (CheckLine(player, x, 0, z, 0, 1, 0, true) || // Horizontal in layer
-                CheckLine(player, 0, y, z, 1, 0, 0, true) || // Vertical in layer
-                CheckLine(player, x, y, 0, 0, 0, 1, true))   // Vertical across layers
-                return true;
-        }
-
-        // Diagonals in layer
-        if (CheckLine(player, 0, 0, z, 1, 1, 0, true) || // Diagonal from top-left to bottom-right in layer
-            CheckLine(player, 0, 3, z, 1, -1, 0, true))  // Diagonal from top-right to bottom-left in layer
-            return true;
-
-        // 3D diagonals
-        if (CheckLine(player, 0, 0, 0, 1, 1, 1, true) || // Diagonal from top-left-front to bottom-right-back
-            CheckLine(player, 3, 0, 0, -1, 1, 1, true) || // Diagonal from top-right-front to bottom-left-back
-            CheckLine(player, 0, 3, 0, 1, -1, 1, true) || // Diagonal from bottom-left-front to top-right-back
-            CheckLine(player, 0, 0, 3, 1, 1, -1, true) || // Diagonal from bottom-left-back to top-right-front
-            CheckLine(player, 3, 3, 0, -1, -1, 1, true) || // Diagonal from bottom-right-front to top-left-back
-            CheckLine(player, 3, 0, 3, -1, 1, -1, true) || // Diagonal from bottom-right-back to top-left-front
-            CheckLine(player, 0, 3, 3, 1, -1, -1, true) || // Diagonal from bottom-right-back to top-left-front
-            CheckLine(player, 3, 3, 3, -1, -1, -1, true))  // Diagonal from bottom-right-back to top-left-front
-            return true;
-
-        return false;
-    }
-
     // Call this method to switch the current player
     void SwitchPlayer()
     {
